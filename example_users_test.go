6b04827391cd763d6c7b71ee5e2d46ce20d18cbf
Joel Noyce Barnham <joelnbarnham@gmail.com>
2020-02-19 08:12:19 +0000

    Update example for new API

--- example_users_test.go
+++ example_users_test.go
@@ -14,19 +14,14 @@ func Example_createUser() {
 		panic(err)
 	}
 
-	db := conn.Database("_users")
-
 	user := sofa.UserDocument{
-		DocumentMetadata: sofa.DocumentMetadata{
-			ID: "org.couchdb.user:Couchy McCouchface",
-		},
-		Name:     "Couchy McCouchface",
-		Password: "example",
-		Roles:    []string{"boat"},
+		Name:     "nkev",
+		Password: "nkev",
+		Roles:    []string{},
 		TheType:  "user",
 	}
 
-	rev, err := db.Put(&user)
+	rev, err := conn.CreateUser(user)
 	if err != nil {
 		panic(err)
 	}
@@ -35,19 +30,34 @@ func Example_createUser() {
 	fmt.Println(strings.Split(rev, "-")[0])
 
 	// Retrieve the document which was created
-	realUser, err := conn.User("org.couchdb.user:Couchy McCouchface", rev)
+	realUser, err := conn.User("nkev", rev)
 	if err != nil {
 		panic(err)
 	}
 
-	delrev, err := db.Delete(&realUser)
+	// Show the whole retrieved user
+	// fmt.Printf("%+v\n", realUser)
+
+	// Modify the roles for the user
+	realUser.Roles = []string{"issue_creator"}
+
+	// Save the modified user
+	updateRev, err := conn.UpdateUser(realUser)
+	if err != nil {
+		panic(err)
+	}
+
+	// Ensure the document has the latest revision so the delete works
+	realUser.DocumentMetadata.Rev = updateRev
+
+	// Delete the user
+	delrev, err := conn.DeleteUser(realUser)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(strings.Split(delrev, "-")[0])
-
 	// Output:
 	// 1
-	// 2
+	// 3
 }
